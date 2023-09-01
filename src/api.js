53ae1504f528c5d91b55ff2c313c60b9e5ca6f3f
ksope <105855785+ksope@users.noreply.github.com>
2023-09-01 19:19:42 +0100

    First commit

--- src/api.js
+++ src/api.js
@@ -87,8 +87,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
-            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
